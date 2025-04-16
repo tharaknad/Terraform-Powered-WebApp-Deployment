365c0672ba566514756c240b55d8b95c85f48c08
Tharak Nad <tharaknadreddy45@gamil.com>
2025-04-16 02:30:10 +0000

    Script for server-2  with Apache and animated HTML page

--- userdata1.sh
+++ userdata1.sh
@@ -5,11 +5,14 @@ apt install -y apache2
 # Get the instance ID using the instance metadata
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
+
+
 # Install the AWS CLI
 apt install -y awscli
 
 # Download the images from S3 bucket
 #aws s3 cp s3://myterraformprojectbucket2023/project.webp /var/www/html/project.png --acl public-read
+
 
 # Create a simple HTML file with the portfolio content and display the images
 cat <<EOF > /var/www/html/index.html
@@ -37,6 +40,7 @@ cat <<EOF > /var/www/html/index.html
 </body>
 </html>
 EOF
+
 
 # Start Apache and enable it on boot
 systemctl start apache2
